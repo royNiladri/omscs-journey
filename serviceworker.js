28143bddd295fb50be7782f6a0f0774476e52a11
Niladri <royNiladri@users.noreply.github.com>
2021-07-09 15:54:34 +0530

    Fix serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -21,7 +21,7 @@ self.addEventListener("install", function (event) {
 // If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function (event) {
   console.log("Request",event.request);
-  if (event.request.method !== "GET" || event.request) return;
+  if (event.request.method !== "GET") return;
 
   event.respondWith(
     fetch(event.request)
